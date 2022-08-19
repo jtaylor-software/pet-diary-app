f08b2c3d566e9749898228317b9c00b11b107009
Jeremy Taylor <jtaylor@jctaylor.org>
2022-08-19 11:02:58 -0500

    Initial Commit

---
+++ PetDairy/PetDairyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetDairyApp.swift
+//  PetDairy
+//
+//  Created by Jeremy Taylor on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct PetDairyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
