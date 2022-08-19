71d1288576cf712311bbab30b239c5809fcb247f
Jeremy Taylor <jtaylor@jctaylor.org>
2022-08-19 11:24:15 -0500

    Create homework/week01 branch Create groups to organize files Add cat images for Onboarding Screen to asset catalog Create placeholder app icon

---
+++ PetDairy/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  PetDairy
+//
+//  Created by Jeremy Taylor on 8/19/22.
+//
+
+import Foundation
+
+enum Constants {
+    enum General {
+        
+    }
+}
---
+++ PetDairy/Views/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PetDairy
+//
+//  Created by Jeremy Taylor on 8/19/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
