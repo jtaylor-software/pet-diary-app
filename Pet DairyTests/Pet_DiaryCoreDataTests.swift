21675298d4a7410c3d05b723265746f0d36934a0
Jeremy Taylor <jtaylor@jctaylor.org>
2022-10-23 16:06:32 -0500

    Unit Test saving to CoreData

--- Pet DairyTests/Pet_DiaryCoreDataTests.swift
+++ Pet DairyTests/Pet_DiaryCoreDataTests.swift
@@ -10,32 +10,49 @@ import XCTest
 
 class PetDiaryCoreDataTest: XCTestCase {
 		var coreDataManager: CoreDataManager!
-		var coreDataPet: CoreDataPet!
+		var petService: PetService!
 		
 		override func setUp() {
 				super.setUp()
 				coreDataManager = TestCoreDataManager()
-				coreDataPet = CoreDataPet(context: coreDataManager.mainContext)
+				petService = PetService(
+						managedObjectContext: coreDataManager.mainContext,
+						coreDataManager: coreDataManager)
 		}
 		
 		override func tearDown() {
 				super.tearDown()
-				coreDataPet = nil
+				petService = nil
 				coreDataManager = nil
 		}
 		
-		func testAddPet() {
-				let pet = coreDataPet
-				coreDataPet.name = "Fluffy"
-				coreDataPet.age = 2
-				coreDataPet.birthday = "7/4/2020"
-				coreDataPet.favoriteToy = "Socks"
-				coreDataPet.trait = "Playful!"
+		func test_AddPet() {
+				let pet = petService.add(name: "Fluffy", favoriteToy: "Socks", imageString: "https://jctaylor.org/fluffy.jpeg", age: 2, birthday: "7/4/2020", trait: "Playful!")
 				
-				XCTAssertNotNil(pet?.name, "name should not be nil")
-				XCTAssertNotNil(pet?.age, "age should not be nil")
-				XCTAssertTrue(pet?.birthday == "7/4/2022")
-				XCTAssertTrue(pet?.favoriteToy == "Socks")
-				XCTAssertTrue(pet?.trait == "Playful!")
+				XCTAssertNotNil(pet.name, "name should not be nil")
+				XCTAssertNotNil(pet.age, "age should not be nil")
+				XCTAssertTrue(pet.imageString == "https://jctaylor.org/fluffy.jpeg")
+				XCTAssertTrue(pet.birthday == "7/4/2020")
+				XCTAssertTrue(pet.favoriteToy == "Socks")
+				XCTAssertTrue(pet.trait == "Playful!")
+		}
+		
+		func test_savePet() {
+				let derivedContext = coreDataManager.newDerivedContext()
+					expectation(
+						forNotification: .NSManagedObjectContextDidSave,
+						object: coreDataManager.mainContext) { _ in
+							return true
+					}
+
+					derivedContext.perform {
+							let pet = self.petService.add(name: "Fluffy", favoriteToy: "Socks", imageString: "https://jctaylor.org/fluffy.jpeg", age: 2, birthday: "7/4/2020", trait: "Playful!")
+
+						XCTAssertNotNil(pet)
+					}
+
+					waitForExpectations(timeout: 2.0) { error in
+						XCTAssertNil(error, "Save did not occur")
+					}
 		}
 }
